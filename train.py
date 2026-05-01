97dbc8bdf6dea82ac79f0e5d733269939cc3ff28
Pekka <75335748+Pekk4@users.noreply.github.com>
2026-05-01 15:31:14 +0300

    Test model update

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import os
 # Load dataset
 X, y = load_wine(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.25, random_state=42
 )
 
 # Start MLflow run
